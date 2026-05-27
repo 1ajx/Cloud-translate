74962b4ae59873593b5701fc85272be6bcd63f1f
Developer <ajx2693482236@gmail.com>
2026-05-28 06:35:06 +0800

    feat: content script entry - message routing to panel

--- src/content/index.js
+++ src/content/index.js
@@ -1,1 +1,32 @@
-// content script stub
+import { MSG } from '../shared/constants.js';
+import { getSelection } from './selection-handler.js';
+import { FloatingPanel } from './floating-panel.js';
+
+const panel = new FloatingPanel();
+
+chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+  switch (message.type) {
+    case MSG.GET_SELECTION: {
+      const result = getSelection();
+      sendResponse(result);
+      break;
+    }
+    case MSG.TRANSLATE_START: {
+      panel.show(message.payload.position);
+      break;
+    }
+    case MSG.STREAM_CHUNK: {
+      panel.appendChunk(message.payload.chunk);
+      break;
+    }
+    case MSG.TRANSLATE_DONE: {
+      panel.done();
+      break;
+    }
+    case MSG.TRANSLATE_ERROR: {
+      panel.showError(message.payload.message);
+      break;
+    }
+  }
+  return false;
+});
